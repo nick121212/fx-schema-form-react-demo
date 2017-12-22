6e3c7142ad29dfebb6a43fa96077ca37c5d8d81b
nick121212 <nick121212@126.com>
2017-12-22 17:42:53 +0800

    demo

--- src/modules/main/footer/component.tsx
+++ src/modules/main/footer/component.tsx
@@ -9,6 +9,9 @@ export class Component extends BaseComponent<any, any> {
 
         return (
             <footer className="pv5 pv6-l ph4 tc">
+                <p>
+                    这里所有的接口方法已经被移除，所以可能影响一部分的表单没办法演示
+                </p>
                 <p className="f6">
                     <span className="dib">
                         <a href="https://github.com/nick121212/fx-schema-form/tree/master/packages/fx-schema-form-react">
